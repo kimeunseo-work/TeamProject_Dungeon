b7e25383bb5fbe563e1a3874e6f88baec3bac1fc
kimeunseo <kimeunseo.work@gmail.com>
2025-10-29 14:05:31 +0900

    set: 빌드에 씬 추가

--- Assets/01.Scripts/Managers/GameManger.cs
+++ Assets/01.Scripts/Managers/GameManger.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManger Instance;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
