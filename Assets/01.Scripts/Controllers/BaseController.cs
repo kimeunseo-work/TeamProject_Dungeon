a2850dd886246f54a1a39c7ddac6676fd5a4ccda
Cheol <ausqkf301@naver.com>
2025-10-29 21:16:13 +0900

    feat(BaseController) : add movement, HandleAction methods

--- Assets/01.Scripts/Controllers/BaseController.cs
+++ Assets/01.Scripts/Controllers/BaseController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Rigidbody _rigidbody;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected virtual void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Start()
     {
-        
+
     }
+
+    protected virtual void Update()
+    {
+        HandleAction();
+        Rotate();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movement(movementDirection);
+    }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    protected virtual void Movement(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        _rigidbody.velocity = direction;
+    }
+
+    protected virtual void Rotate(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    }
+
 }
