25ec29e84d32cacb4feb967ab01fef2404e9ad90
Cheol <ausqkf301@naver.com>
2025-10-30 17:58:46 +0900

    feat : Add MonsterController / DistanceToTarget, DirectionToTarget, HaldleAction methods

--- Assets/01.Scripts/Controllers/MonsterController.cs
+++ Assets/01.Scripts/Controllers/MonsterController.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterController : MonoBehaviour
+public class MonsterController : BaseController
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float followRange = 15f;
+
+    //public void Init(Transform target)
+    //{
+    //    this.target = target;
+    //}
+
+    protected float DistanceToTarget()
     {
-        
+        return Vector3.Distance(transform.position, target.position);
+        // 두 포지션 사이 거리
     }
 
-    // Update is called once per frame
-    void Update()
+    protected Vector2 DirectionToTarget()
     {
-        
+        return (target.position - transform.position).normalized;
+    }
+
+    protected override void HandleAction()
+    {
+        base.HandleAction();
+
+        if(target == null)
+        {
+            if (!movementDirection.Equals(Vector2.zero)) movementDirection = Vector2.zero;
+            return;
+        }
+        float distance = DistanceToTarget();
+        Vector2 direction = DirectionToTarget();
+
+        if (distance < followRange)
+        {
+            lookDirection = direction;
+
+            movementDirection = direction;
+        }
     }
 }
