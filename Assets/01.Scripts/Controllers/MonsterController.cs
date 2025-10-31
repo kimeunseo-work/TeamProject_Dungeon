c4154a7db18c9716c158b6be74c8182373161a0b
Cheol <ausqkf301@naver.com>
2025-10-31 15:15:42 +0900

    fix : Change access from private to public

--- Assets/01.Scripts/Controllers/MonsterController.cs
+++ Assets/01.Scripts/Controllers/MonsterController.cs
@@ -8,7 +8,7 @@ public class MonsterController : BaseController
 
     private Transform target;
     private float followRange = 15f;
-    private float attackRange = 0.8f;
+    public float attackRange = 0.8f;
     public float speed = 1f;
 
     /*√ ±‚»≠*/
