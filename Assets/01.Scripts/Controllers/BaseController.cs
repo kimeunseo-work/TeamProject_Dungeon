11f1c2b06603f769d173a553bd0bb8706e44ddf6
kimeunseo <kimeunseo.work@gmail.com>
2025-10-31 11:42:14 +0900

    feat: 컨트롤러에 IsMove 추가

--- Assets/01.Scripts/Controllers/BaseController.cs
+++ Assets/01.Scripts/Controllers/BaseController.cs
@@ -17,11 +17,13 @@ public class BaseController : MonoBehaviour
     public Vector2 MovementDirection { get { return movementDirection; } }
 
     protected Vector2 knockback = Vector2.zero;
-    protected float  knockbackDuration = 0f;
+    protected float knockbackDuration = 0f;
 
     protected Vector2 lookDirection = Vector2.zero;
     public Vector2 LookDirection { get { return lookDirection; } }
 
+    public bool IsMove { get; protected set; } = false;
+    protected float Speed = 5f;
 
     /*생명 주기*/
     //=======================================//
@@ -40,7 +42,6 @@ public class BaseController : MonoBehaviour
 
     protected virtual void Update()
     {
-        //HandleAction();
         Rotate(movementDirection);
     }
 
@@ -69,7 +70,11 @@ public class BaseController : MonoBehaviour
 
     protected virtual void Movement(Vector2 direction)
     {
-        direction = direction * 5;
+        // 이동하지 않으면
+        if (!IsMove) direction = default;
+
+        direction = direction * Speed;
+
         if (knockbackDuration > 0.0f)
         {
             direction *= 0.2f;
@@ -81,11 +86,11 @@ public class BaseController : MonoBehaviour
 
     protected virtual void Rotate(Vector2 direction)
     {
+        if (!IsMove) direction = default;
+
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         bool isLeft = Mathf.Abs(rotZ) > 90;
 
-        if (movementDirection == default) return;
-        
         charRenderer.flipX = isLeft;
     }
 }
