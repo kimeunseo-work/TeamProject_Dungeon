f351e3938077601437ac591d2995e853574feb23
adelipingu14 <adelipingu14@gmail.com>
2025-11-03 17:49:25 +0900

    Set: 오디오매니저 세팅

--- Assets/01.Scripts/Managers/AudioManager.cs
+++ Assets/01.Scripts/Managers/AudioManager.cs
@@ -1,16 +1,93 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class AudioManager : MonoBehaviour 
+public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static AudioManager instance;
+    private AudioSource audioSource;
+
+    [Header("BGM Clips")]
+    public AudioClip startBGM;   // LobbyScene
+    public AudioClip normalBGM;  // DungeonScene / 기본 진행
+    public AudioClip clearBGM;   // 스테이지 클리어
+    public AudioClip failBGM;    // 패배 연출
+    public AudioClip bossBGM;    // 보스 스테이지
+
+    [Header("SFX Clips")]
+    public AudioClip arrowShot;
+    public AudioClip hitSFX;
+    public AudioClip clickSFX;
+
+    private void Awake()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        
+        if (scene.name == "LobbyScene")
+        {
+            PlayBGM(startBGM);
+        }
+        else if (scene.name == "DungeonScene")
+        {
+            PlayBGM(normalBGM);
+        }
+    }
+
+    //BGM 컨트롤
+    public void PlayBGM(AudioClip clip)
+    {
+        if (clip == null) return;
+        if (audioSource.clip == clip) return;
+
+        audioSource.clip = clip;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    public void PlayBossBGM()
+    {
+        PlayBGM(bossBGM);
+    }
+
+    public void PlayClearBGM()
+    {
+        audioSource.clip = clearBGM;
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+
+    public void PlayFailBGM()
+    {
+        audioSource.clip = failBGM;
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+
+    // SFX 매서드
+    public void PlayShot()
+    {
+        audioSource.PlayOneShot(arrowShot);
+    }
+
+    public void PlayHit()
+    {
+        audioSource.PlayOneShot(hitSFX);
+    }
+
+    public void PlayClick()
+    {
+        audioSource.PlayOneShot(clickSFX);
     }
 }
