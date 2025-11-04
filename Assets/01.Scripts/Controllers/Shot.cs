b97ce26cca2fcc6ebbd52e5d84f7d60fa49a8fc7
bbbp98 <qjarms1300@gmail.com>
2025-11-04 19:00:01 +0900

    feat: 몬스터 투사체(해골) 파티클

--- Assets/01.Scripts/Controllers/Shot.cs
+++ Assets/01.Scripts/Controllers/Shot.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Shot : MonoBehaviour
 {
     public float lifeTime = 10f;
     [SerializeField] int damage;
-
+    [SerializeField] ParticleSystem particle;
     private void Start()
     {
         Destroy(gameObject, lifeTime);
@@ -16,7 +17,35 @@ public class Shot : MonoBehaviour
         {
             // 데미지 처리
             collision.GetComponent<Player>().TakeDamage(damage);
+
+            //gameObject.SetActive(false);
+            SpawnParticle();
             Destroy(gameObject);
+            return;
+        }
+
+        if (collision.CompareTag("Wall"))
+        {
+            //gameObject.SetActive(false);
+            SpawnParticle();
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void SpawnParticle()
+    {
+        if (particle != null)
+        {
+            ParticleSystem instance = Instantiate(
+                particle,
+                transform.position,   // 현재 총알 위치
+                Quaternion.identity   // 회전 없음
+            );
+
+            instance.transform.parent = null;
+            instance.Play();
+            Destroy(instance.gameObject, instance.main.duration);
         }
     }
 }
